1aa1c74498f512d3d28b57031d8c453158103c62
Samuel Factor <sfxfactor@gmail.com>
2015-11-20 12:06:24 -0600

    new SETLLAR directory. moved 0.3, 1, 3 Msun models to otherModels directory. created 100 Msun initial condition files. This probably breaks the code since I havent fixed the opacities.

--- fig1.py
+++ fig1.py
@@ -8,14 +8,17 @@ M = (np.arange(10)+1.)*100.
 Z = np.array([1e-9,0.02])
 
 R1 = 10.*Rsun*((M/370.)*((Z[0]/1e-9)**0.2))**(1./2.2)
-print M[0]
-print R1[0]/Rsun
+print "M [Msun] = ", M[0]
+print "R [Rsun] = ", R1[0]/Rsun
 R2 = 10.*Rsun*((M/370.)*((Z[1]/1e-9)**0.2))**(1./2.2)
 Ledd = 1.25e38*M
+print "L [erg s^-1] = ", Ledd[0]
 TeffL1 = (Ledd/(4.*np.pi*sig*R1**2))**0.25
+print "T [K] (from Ledd)  = ", TeffL1[0]
 TeffL2 = (Ledd/(4.*np.pi*sig*R2**2))**0.25
 
 Teff1 = (1.1e5)*((Z[0]/1e-9)**-0.05)*((M/100.)**0.025)
+print "T [K] (from M) = ", Teff1[0]
 Teff2 = (1.1e5)*((Z[1]/1e-9)**-0.05)*((M/100.)**0.025)
 
 plt.plot(np.log10(TeffL1),np.log10(Ledd/Lsun),"D-")
